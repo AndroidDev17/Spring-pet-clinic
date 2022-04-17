51915022c09b81b77c6df2f6a16a0b4977966dd6
Ankit <ankit99966@gmail.com>
2022-04-17 19:13:47 +0530

    create owners index page. closes #17

--- pet-clinic-web/src/main/java/com/ankit/springpetclinic/contollers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ankit/springpetclinic/contollers/OwnerController.java
@@ -1,6 +1,8 @@
 package com.ankit.springpetclinic.contollers;
 
+import com.ankit.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -11,8 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("owners")
 public class OwnerController {
 
+    private final OwnerService service;
+
+    public OwnerController(OwnerService service) {
+        this.service = service;
+    }
+
     @RequestMapping({"","/","/index","index.html"})
-    public String getAllOwners() {
+    public String getAllOwners(Model model) {
+        model.addAttribute("owners",service.findAll());
         return "owners/index";
     }
 }
